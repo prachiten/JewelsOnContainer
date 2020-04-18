23efb2e69afa336ddbbb0436ce9b3fb48a13173c
prachiten <61093158+prachiten@users.noreply.github.com>
2020-04-18 02:53:29 -0700

    swagger added

--- ProductCalatog/Startup.cs
+++ ProductCalatog/Startup.cs
@@ -35,9 +35,25 @@ namespace ProductCalatog
             var connectionString = $"Server={server};Database={database};User Id={user};Password={password}";
             Console.WriteLine(connectionString);
             services.AddDbContext<CatalogContext>(options=>options.UseSqlServer(connectionString));
-           // services.AddDbContext<CatalogContext>(options => options.UseSqlServer(Configuration["Connectionstring"]));  // For IISxpress
+            // services.AddDbContext<CatalogContext>(options => options.UseSqlServer(Configuration["Connectionstring"]));  // For IISxpress
             //above statement creates a table of type CatalogContext 
             //ie it creates a object of CatalogContext and while creating object we need to pass option in its constructor
+
+            //-------------------ADD SWAGGER--------------------------------------
+            services.AddSwaggerGen(options =>
+            {
+                //options.DescribeAllEnumsAsStrings();
+                //v1 is version 1
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "JewelsonContainers - Product Catalog API",//title of documentation
+                    Version = "v1",                                   //this is actual version
+                    Description = "Product catalog microservice"      //description of document
+                });
+                //
+                options.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +67,13 @@ namespace ProductCalatog
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseSwagger()        //swagger will have UI page as well where all documented information will be available
+                //type http://localhost:6803/swagger in browser to see swagger documentation
+               .UseSwaggerUI(e =>
+               {
+                   // give swagger url and name of document
+                   e.SwaggerEndpoint($"/swagger/v1/swagger.json", "ProductCatalogAPI V1");
+               });
 
             app.UseEndpoints(endpoints =>
             {
